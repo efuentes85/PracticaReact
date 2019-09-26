3dad5842a7d238451266e170f94197241439b546
Eduardo Fuentes Velasco <efuentes85@gmail.com>
2019-09-26 02:39:43 +0000

    Ejercicios terminados!

--- exercises/02.6-conditional-rendering/index.js
+++ exercises/02.6-conditional-rendering/index.js
@@ -4,7 +4,17 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//your component here
+
+	let alertClasses = "alert";
+	if (props.color == "red") alertClasses += " alert-danger";
+	else if (props.color == "orange") alertClasses += " alert-warning";
+	return (
+		<div className={alertClasses} role="alert">
+			{props.text}
+		</div>
+	);
 };
+
 Alert.propTypes = {
 	color: PropTypes.string,
 	text: PropTypes.string
